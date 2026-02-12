0af88d3b1b4421aac9b81891a12582b1cd3506b5
Nikhil Reddy <s.nikhilreddy3446@gmail.com>
2026-02-12 10:30:57 +0530

    Add implementation for happy number check

---
+++ Happynumber1.java
@@ -0,0 +1,26 @@
+import java.util.HashSet;
+import java.util.Set;
+
+class Solution {
+    public boolean isHappy(int n) {
+        Set<Integer> seen = new HashSet<>();
+        
+        // Keep going until n becomes 1 or we hit a number we've seen before
+        while (n != 1 && !seen.contains(n)) {
+            seen.add(n);
+            n = getNext(n);
+        }
+        
+        return n == 1;
+    }
+
+    private int getNext(int n) {
+        int totalSum = 0;
+        while (n > 0) {
+            int d = n % 10;
+            n = n / 10;
+            totalSum += d * d;
+        }
+        return totalSum;
+    }
+}
