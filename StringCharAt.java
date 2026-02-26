4e0e04b241ff36e08ed59a7b9685bd7a0dc25d57
Nikhil Reddy <s.nikhilreddy3446@gmail.com>
2026-02-26 21:59:35 +0530

    Create CharAtExample to show charAt() usage
    Added example to demonstrate the use of charAt() method.

---
+++ StringCharAt.java
@@ -0,0 +1,13 @@
+public class CharAtExample {
+    public static void main(String[] args) {
+        String word = "Java";
+
+        // Get the first character
+        char first = word.charAt(0); // 'J'
+        
+        // Get the third character
+        char third = word.charAt(2); // 'v'
+
+        System.out.println("First letter: " + first);
+    }
+}
