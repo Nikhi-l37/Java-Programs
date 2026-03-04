fb67e07020815cbef7c6934912ae0a4d72b57616
Nikhil Reddy <s.nikhilreddy3446@gmail.com>
2026-03-04 23:16:29 +0530

    Implement method to count special positions in matrix

---
+++ Special Positions in a Binary Matrix.java
@@ -0,0 +1,31 @@
+class Solution {
+    public int numSpecial(int[][] mat) {
+        int m = mat.length;
+        int n = mat[0].length;
+        int[] rowCount = new int[m];
+        int[] colCount = new int[n];
+        
+        // Step 1: Pre-calculate the number of 1s in each row and column
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (mat[i][j] == 1) {
+                    rowCount[i]++;
+                    colCount[j]++;
+                }
+            }
+        }
+        
+        int specialPositions = 0;
+        
+        // Step 2: Check which (i, j) satisfies the special condition
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (mat[i][j] == 1 && rowCount[i] == 1 && colCount[j] == 1) {
+                    specialPositions++;
+                }
+            }
+        }
+        
+        return specialPositions;
+    }
+}
